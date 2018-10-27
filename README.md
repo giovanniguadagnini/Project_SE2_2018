1f49327f5152890fc6f871dea7e4e9c105ea504e
Devis <devisdalmoro@gmail.com>
2018-10-27 13:09:03 +0200

    Added some comments in api.js & members name in README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Project_SE2_2018
 Group repository for Exam Platform Project of the Software Engineering 2 course
+Members:
+    - Boffo Marco
+    - Dal Moro Devis
+    - Dissegna Daniel
+    - Guadagnini Giovanni
+    - Tessarolo Davide
--- api.js
+++ api.js
@@ -1,7 +1,9 @@
+//import modules / ext. libraries
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => console.log('Listening on port '+ PORT))
-
+//behaviour to attend for GET request to "/"
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(PORT, () => console.log('Listening on port '+ PORT))
