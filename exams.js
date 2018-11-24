389ef4f9f20253a138f5ecf39ff8c030dc05558d
Daniel <ddissegna@gmail.com>
2018-11-24 21:42:51 +0100

    added /exams and /exams/:id methods with relatives filters

---
+++ exams.js
@@ -0,0 +1,118 @@
+const app = require('./app');
+const userDao = require('./userDao');
+app.route('/exams')
+    .get(function(req, res) { //Get all managable exams
+        var sortStudBy = req.query.sortStudBy;
+        if(sortStudBy == null){
+            sortStudBy ="enrol";
+        }
+        var minStudByMark = req.query.minStudByMark;
+        if(minStudByMark == null) {
+            minStudByMark = -1;
+        }
+        var maxStudByMark = req.query.maxStudByMark;
+        if(maxStudByMark == null) {
+            maxStudByMark = 31;
+        }
+        var taskType = req.query.taskType;
+        if(taskType == null) {
+            taskType = 1111;
+        }
+        var exams = userDao.getAllExams(sortStudBy, minStudByMark,maxStudByMark,taskType);
+        if(exams != null) {
+            res.status(200).send(exams);
+        } else {
+            res.status(400).send("Bad request");
+        }
+    })
+    .post(function(req, res) { //Create an exam
+        var exam = {
+          id: req.body.id,
+          name : req.body.name,
+          owner : req.body.owner,
+          teachers : req.body.teachers,
+          students : req.body.students,
+          tasks : req.body.tasks,
+          submissions : req.body.submissions,
+          deadline : req.body.deadline,
+          reviewable : req.body.reviewable,
+          num_shuffle : req.body.num_shuffle
+        };
+        exam = userDao.createExam(exam);//trying to create the exam
+        if (exam != null) {
+            res.status(200).send(exam);
+        } else {
+            res.status(405).send("Invalid input");
+        }
+    });
+
+app.route('/exams/:id')
+    .get(function(req, res) {
+        var id = req.id;
+        if(Number.isInteger(id) == true) {
+            var sortStudBy = req.query.sortStudBy;
+            if(sortStudBy == null){
+                sortStudBy ="enrol";
+            }
+            var minStudByMark = req.query.minStudByMark;
+            if(minStudByMark == null) {
+                minStudByMark = -1;
+            }
+            var maxStudByMark = req.query.maxStudByMark;
+            if(maxStudByMark == null) {
+                maxStudByMark = 31;
+            }
+            var taskType = req.query.taskType;
+            if(taskType == null) {
+                taskType = 1111;
+            }
+            var exam = userDao.getExam(id,sortStudBy, minStudByMark,maxStudByMark,taskType);
+            if (exam != null) {
+                res.status(200).send(exam);
+            } else {
+                res.status(404).send("Exam not found");
+            }
+        }else{
+            res.status(400).send("Invalid ID");
+        }
+    })
+    .put(function(req, res) {
+        var id = req.id;
+        if(Number.isInteger(id) == true) {
+            var exam = {
+              id: req.body.id,
+              name : req.body.name,
+              owner : req.body.owner,
+              teachers : req.body.teachers,
+              students : req.body.students,
+              tasks : req.body.tasks,
+              submissions : req.body.submissions,
+              deadline : req.body.deadline,
+              reviewable : req.body.reviewable,
+              num_shuffle : req.body.num_shuffle
+            };
+            exam = userDao.updateExam(exam);
+            if (exam != null) {
+                res.status(200).send(exam);
+            } else {
+                res.status(404).send("Exam not found");
+            }
+        }else{
+            res.status(400).send("Invalid ID");
+        }
+
+    })
+    .delete(function(req, res) {
+        var id = req.id;
+        if(Number.isInteger(id) == true) {
+            exam = userDao.deleteExam(id);
+            if (exam != null) {
+                res.status(200).send(exam);
+            } else {
+                res.status(404).send("Exam not found");
+            }
+        }else{
+            res.status(400).send("Invalid ID");
+        }
+
+    });;
