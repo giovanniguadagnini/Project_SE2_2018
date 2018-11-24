170f21e00534df066dade2b26c9acd1c313b57cb
texdade <davidetessarolo@gmail.com>
2018-11-25 00:07:36 +0100

    Started implementation of userGroup.js

---
+++ userGroups.js
@@ -0,0 +1,16 @@
+app.route('/userGroups')
+  .get(function(req, res) {
+      var sortingMethod = req.query.sortUsrBy;
+      //if the sorting method is not specified or not recognized, switch to the default one
+      if(sortingMethod==null || (sortingMethod != "enrol" || sortingMethod != "alpha"))
+          sortingMethod="enrol";
+      var userGroups = getAllUserGroups(sortingMethod);
+      if(userGroups != null)
+          res.status(200).send(JSON.stringify(userGroups));
+      else
+          res.status(404).send("No userGroup found");
+  })
+  .post(function(req, res){
+      
+  });
+
