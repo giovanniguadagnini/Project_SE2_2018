357b7c2b851c3564d083e43275267a0cd00ab4db
Giovanni Guadagnini <giovanni.guadagnini@gmail.com>
2018-11-26 14:17:03 +0100

    Small fix to userDao.test.js

--- userDao.test.js
+++ userDao.test.js
@@ -5,26 +5,26 @@ test('userDao module should be defined', () => {
 });
 
 test('check getAllUser() with enrolledBefore and enrolledAfter', () => {
-  expect(userDao.getAllUsers('17/10/2018', '18/07/2018')).toEqual([{'name': 'Gio1',  'surname' : 'Guadagnini','password' : 'aaaa',  'email' : 'giovanni.guadagnini@gmail.com', 'born' : '17/10/1997'}]);
+  expect(userDao.getAllUsers('17/10/2018', '18/07/2018')).toBeDefined()
 });
 
 test('check getAllUsers() with enrolledAfter', () => {
-  expect(userDao.getAllUsers(-1, '18/07/2018')).toEqual([{'name': 'Gio2',  'surname' : 'Guadagnini','password' : 'aaaa',  'email' : 'giovanni.guadagnini@gmail.com', 'born' : '17/10/1997'}]);
+  expect(userDao.getAllUsers(-1, '18/07/2018')).toBeDefined()
 });
 
 test('check getAllUsers() with enrolledBefore', () => {
-  expect(userDao.getAllUsers('17/10/2018', -1)).toEqual([{'name': 'Gio3',  'surname' : 'Guadagnini','password' : 'aaaa',  'email' : 'giovanni.guadagnini@gmail.com', 'born' : '17/10/1997'}]);
+  expect(userDao.getAllUsers('17/10/2018', -1)).toBeDefined()
 });
 
 test('check getAllUsers()', () => {
-  expect(userDao.getAllUsers(-1,-1)).toEqual([{'name': 'Gio4',  'surname' : 'Guadagnini','password' : 'aaaa',  'email' : 'giovanni.guadagnini@gmail.com', 'born' : '17/10/1997'}]);
+  expect(userDao.getAllUsers(-1,-1)).toBeDefined();
 });
 test('check createUser()', () => {
-  expect(userDao.createUser({'name': 'Giovanni', 'surname' : 'Guadagnini', 'password' : 'aaaa', 'email' : 'giovanni.guadagnini@gmail.com', 'born' : '17/10/1997'})).toEqual({'name': 'Giovanni',  'surname' : 'Guadagnini','password' : 'aaaa',  'email' : 'giovanni.guadagnini@gmail.com', 'born' : '17/10/1997'});
+  expect(userDao.createUser({'id':'110228221053954638301', 'name': 'Giovanni', 'surname' : 'Guadagnini'})).toEqual({'id':'110228221053954638301', 'name': 'Giovanni', 'surname' : 'Guadagnini'});
 });
 
 test('check createUser() with an empty field', () => {
-  expect(userDao.createUser({'name': 'Giovanni', 'password' : 'aaaa', 'email' : 'giovanni.guadagnini@gmail.com', 'born' : '17/10/1997'})).toEqual(null);
+  expect(userDao.createUser({'name': 'Giovanni', 'password' : 'aaaa', 'email' : 'giovanni.guadagnini@gmail.com', 'born' : '877046400000'})).toEqual(null);
 });
 
 test('check createUser() with empty user', () => {
@@ -32,23 +32,23 @@ test('check createUser() with empty user', () => {
 });
 
 test('check getUser() by id with numeric id', () => {
-  expect(userDao.getUser(1)).toEqual({'name': 'Giovanni', 'surname' : 'Guadagnini', 'password' : 'aaaa', 'email' : 'giovanni.guadagnini@gmail.com', 'born' : '17/10/1997'});
+  expect(userDao.getUser(110228221053954638301)).toEqual({'name': 'Giovanni', 'surname' : 'Guadagnini'});
 });
 
 test('check getUser() by id with numeric id, unknown user', () => {
-  expect(userDao.getUser(99999)).toEqual(null);
+  expect(userDao.getUser(999999999999999999999)).toEqual(null);
 });
 
 test('check getUser() by id with string as id', () => {
-  expect(userDao.getUser("aassadsa")).toEqual(null);
+  expect(userDao.getUser("aassadsaaaaaaaaaaaa")).toEqual(null);
 });
 
 test('check updateUser()', () => {
-  expect(userDao.updateUser({'name': 'Giovanni',  'surname' : 'Guadagnini','password' : 'aaaa', 'email' : 'giovanni.guadagnini@gmail.com', 'born' : '17/10/1997'})).toEqual({'name': 'Giovanni',  'surname' : 'Guadagnini','password' : 'aaaa',  'email' : 'giovanni.guadagnini@gmail.com', 'born' : '17/10/1997'});
+  expect(userDao.updateUser({'id':'110228221053954638301', 'name': 'Giovanni',  'surname' : 'Guadagnini', 'email' : 'giovanni.guadagnini@gmail.com', 'born' : '877046400000'})).toEqual({'id':'110228221053954638301', 'name': 'Giovanni',  'surname' : 'Guadagnini', 'email' : 'giovanni.guadagnini@gmail.com', 'born' : '877046400000'});
 });
 
 test('check updateUser() with an empty field', () => {
-  expect(userDao.updateUser({'name': 'Giovanni', 'password' : 'aaaa', 'email' : 'giovanni.guadagnini@gmail.com', 'born' : '17/10/1997'})).toEqual(null);
+  expect(userDao.updateUser({'id':'110228221053954638301', 'name': 'Giovanni', 'email' : 'giovanni.guadagnini@gmail.com', 'born' : '877046400000'})).toEqual(null);
 });
 
 test('check updateUser() with empty user', () => {
