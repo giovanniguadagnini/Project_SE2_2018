32e3351bb9b281009de69160d69ec569021300cf
Devis <devisdalmoro@gmail.com>
2018-11-26 22:21:50 +0100

    Rebase

--- google-auth.js
+++ google-auth.js
@@ -1,5 +1,6 @@
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const passport = require('passport');
+const request = require('request');
 const { OAuth2Client } = require('google-auth-library');
 const { Strategy } = require('passport-http-bearer');
 
@@ -11,14 +12,13 @@ const { User } = require('./db');
  * @param {Object} app
  */
 exports.auth = app => {
-  app.use(passport.initialize());
-
-  if (process.env.NODE_ENV !== 'test') {
-    registerGoogleAuth(app);
-    registerBearerAuth();
-  } else {
-    registerBearerMock();
-  }
+    app.use(passport.initialize());
+    if (process.env.NODE_ENV !== 'test') {//real case
+        registerGoogleAuth(app);
+        registerBearerAuth();
+    } else {//test case
+        registerBearerMock();
+    }
 };
 
 /**
@@ -32,39 +32,39 @@ exports.protect = () => {
 // http://www.passportjs.org/docs/google/
 // https://github.com/jaredhanson/passport-google-oauth2
 const registerGoogleAuth = app => {
-  passport.serializeUser((user, done) => {
-    done(null, user);
-  });
-  passport.deserializeUser((user, done) => {
-    done(null, user);
-  });
+    passport.serializeUser((user, done) => {
+        done(null, user);
+    });
+    passport.deserializeUser((user, done) => {
+        done(null, user);
+    });
 
-  // we register the google strategy
-  passport.use(
-    new GoogleStrategy(
-      {
-        clientID: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: process.env.CALLBACK_URL
-      },
-      (token, refreshToken, profile, done) => {
-        // here we would store the user information in the db, if the user does not exist.
-        let user = User.findOrCreate(profile);
-        return done(null, {
-          user,
-          token
-        });
-      }
-    )
-  );
+    // we register the google strategy
+    passport.use(
+        new GoogleStrategy(
+            {
+                clientID: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+                callbackURL: process.env.CALLBACK_URL
+            },
+            (token, refreshToken, profile, done) => {
+                // here we would store the user information in the db, if the user does not exist.
+                let user = User.findOrCreate(profile);
+                return done(null, {
+                  user,
+                  token
+                });
+            }
+        )
+    );
 
-  // This is the endpoint for authentication using google
-  app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-      scope: ['https://www.googleapis.com/auth/userinfo.profile']
-    })
-  );
+    // This is the endpoint for authentication using google
+    app.get('/auth/google',
+        passport.authenticate('google', {
+            scope: ['https://www.googleapis.com/auth/userinfo.profile']
+        })
+    );
+
 
   // Google after successful login call this endpoint. We return
   // the token that should be used to invoke the rest of the API.
@@ -90,33 +90,41 @@ const registerBearerAuth = () => {
   //    https://developers.google.com/identity/sign-in/web/backend-auth
   passport.use(
     new Strategy(async (token, cb) => {
-      try {
-        const client = new OAuth2Client(
-          process.env.CLIENT_ID,
-          process.env.CLIENT_SECRET,
-          ''
-        );
-        const tokenInfo = await client.getTokenInfo(token);
-        const user = User.findOrCreate({ id: tokenInfo.sub });
-        return cb(null, user);
-      } catch (error) {
-        console.error(error);
-        return cb(null, false);
-      }
+        try {
+            const client = new OAuth2Client(
+                process.env.CLIENT_ID,
+                process.env.CLIENT_SECRET,
+                ''
+            );
+            const tokenInfo = await client.getTokenInfo(token);
+            const user = User.findOrCreate({ id: tokenInfo.sub });
+            return cb(null, user);
+        } catch (error) {
+            console.error(error);
+            return cb(null, false);
+        }
     })
   );
 };
 
 const registerBearerMock = () => {
-  passport.use(
-    new Strategy(async (token, cb) => {
-      try {
-        const user = User.findOrCreate({ id: token });
-        return cb(null, user);
-      } catch (error) {
-        console.error(error);
-        return cb(null, false);
-      }
-    })
-  );
+    passport.use(
+        new Strategy(async (token, cb) => {
+            try {
+                const user = User.findOrCreate({ id: token });
+                return cb(null, user);
+            } catch (error) {
+                console.error(error);
+                return cb(null, false);
+            }
+        })
+    );
 };
+
+function checkUser(id){
+    if(dbcheck(id) == true){//already registered
+        return true;
+    }else{
+        return false;
+    }
+}
