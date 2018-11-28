8eb8c8360523a4404ed29e328080b67ba4a845ab
Devis <devisdalmoro@gmail.com>
2018-11-28 11:48:15 +0100

    Fixed commit

--- devisExperiments.js
+++ devisExperiments.js
@@ -1,3 +1,5 @@
 const userDao = require('./userDao');
 
-userDao.getUser({}, '123').then(value => {console.log(JSON.stringify(value.born.year));});
+userDao.getUser({}, '123').then(value => {
+    console.log(JSON.stringify(value.born));
+});
