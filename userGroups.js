da0ba0cfb09c7509f423cd75cec3054c53a57819
texdade <davidetessarolo@gmail.com>
2018-12-04 10:43:17 +0100

    Implemented update and delete userGroup (still untested) Security checks are still to be implemented
    refs=#17, #18

--- userGroups.js
+++ userGroups.js
@@ -1,8 +1,7 @@
-const app = require('./app');
 const userGroupsDao = require('./userGroupsDao')
 const userDao = require('./userDao');
 
-function postUserGroup(req, res){
+function createUserGroup(req, res){
     userDao.getUser(req.user, req.user.id).then( g_creator => {
         let userGroup = {
             creator : g_creator,
@@ -24,8 +23,6 @@ function getUserGroup(req, res){
     let id = req.id;
     if(Number.isInteger(+id)) {
         let sortingMethod = req.query.sortStudBy;
-        if(sortingMethod==null || (sortingMethod != 'enrol' || sortingMethod != 'alpha'))
-            sortingMethod='enrol';
         userGroupsDao.getUserGroup(req.user, id, sortingMethod).then( userGroup => {
             if(userGroup!=null)
                 res.status(200).json(userGroup);
@@ -38,87 +35,43 @@ function getUserGroup(req, res){
     }
 }
 
-
-module.exports = {postUserGroup, getUserGroup};
-/*
-app.route('/userGroups')
-//get all userGroups sorted by one of two methods
-    .get(function(req, res) {
-        var sortingMethod = req.query.sortUsrBy;
-        //if the sorting method is not specified or not recognized, switch to the default one
-        if(sortingMethod==null || (sortingMethod != 'enrol' || sortingMethod != 'alpha'))
-            sortingMethod='enrol';
-        var userGroups = userGroupsDao.getAllUserGroups(sortingMethod);
+function getAllUserGroups(req, res){
+    let sortingMethod = req.query.sortStudBy;
+    userGroupsDao.getAllUserGroups(req.user, sortingMethod).then(userGroups => {
         if(userGroups != null)
-            res.status(200).json(JSON.stringify(userGroups));
+            res.status(200).json(userGroups);
         else
             res.status(404).send('No userGroup found');
-    })
+    });
+}
 
-    //create a new userGroup
-    .post(function(req, res){
-        var userGroup = {
-            id : req.body.id,
-            creator : req.body.creator,
-            name : req.body.name,
-            users : req.body.users
-        };
-        userGroup = userGroupsDao.createUserGroup(userGroup);
-        if(userGroup != null)
+function updateUserGroup(req, res){
+    let userGroup = {
+        id: req.body.id,
+        creator: req.body.creator,
+        name: req.body.name,
+        users: req.body.users
+    };
+
+    userGroupsDao.updateUserGroup(userGroup).then(userGroup => {
+        if(userGroup!=null)
             res.status(200).json(userGroup);
         else
-            res.status(405).send('Invalid input');
+            res.status(404).send('userGroup not found');
     });
+}
 
-app.route('/userGroups/:id')
-//get the userGroup with id {id}, can choose how the users get ordered
-    .get(function(req,res){
-        var id = req.id;
-        if(Number.isInteger(id) == true) {
-            var sortingMethod = req.query.sortStudBy;
-            if(sortingMethod==null || (sortingMethod != 'enrol' || sortingMethod != 'alpha'))
-                sortingMethod='enrol';
-            var userGroup = userGroupsDao.getUserGroup(id, sortingMethod);
+function deleteUserGroup(req, res){
+    let id = req.body.id;
+    if(Number.isInteger(id)){
+        userGroupsDao.deleteUserGroup(req.user, id).then( userGroup => {
             if(userGroup!=null)
                 res.status(200).json(userGroup);
             else
                 res.status(404).send('userGroup not found' );
-        } else {
-            res.status(400).send('Invalid ID supplied');
-        }
-    })
+        });
+    } else
+        res.status(400).send('Bad request');
+}
 
-    //update the userGroup with id {id}
-    .put(function(req,res){
-        var id = req.id;
-        if(Number.isInteger(id) == true) {
-            var userGroup = {
-                id : req.body.id,
-                creator : req.body.creator,
-                name : req.body.name,
-                users : req.body.users
-            };
-            userGroup = userGroupsDao.updateUserGroup(userGroup);
-            if(userGroup!=null)
-                res.status(200).json(userGroup);
-            else
-                res.status(404).send('userGroup not found');
-        } else {
-            res.status(400).send('Invalid ID supplied');
-        }
-    })
-
-    //delete the userGroup with id {id}
-    .delete(function(req,res){
-        var id = req.id;
-        if(Number.isInteger(id) == true){
-            userGroup = userGroupsDao.deleteUserGroup(id);
-            if(userGroup!=null)
-                res.status(200).json(userGroup);
-            else
-                res.status(404).send('userGroup not found');
-        } else {
-            res.status(400).send('Invalid ID supplied');
-        }
-    });
-*/
+module.exports = {createUserGroup, getAllUserGroups, getUserGroup, updateUserGroup, deleteUserGroup};
