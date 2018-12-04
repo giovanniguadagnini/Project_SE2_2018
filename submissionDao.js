91757eab5024893aff9e81582153bac57d44faac
Devis <devisdalmoro@gmail.com>
2018-12-04 10:49:17 +0100

    First comm subm

---
+++ submissionDao.js
@@ -0,0 +1,19 @@
+const utilities = require('./utilities');
+const connection = utilities.connection;
+
+
+function getAllSubmissions(id_user){
+    return [{}];
+}
+
+function getSubmissionById(loggedUser, idSubmission{
+    return new Promise( resolve => {
+
+    });
+}
+
+function updateSubmission(submission){
+    return submission;
+}
+
+module.exports = {getAllSubmissions, getSubmissionById, updateSubmission};
